4a54ae49c6a28383d0c4aabc727b9fe2d8811bbb
aleksandar <acaacaas@gmail.com>
2021-05-10 12:28:14 +0200

    dotnet project

---
+++ Project_1/SOA/SOA/Models/SolarPrediction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOA.Models
+{
+    public class SolarPrediction
+    {
+        public long UNIXTime { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public float Radiation { get; set; }
+        public int Temperature { get; set; }
+        public float Pressure { get; set; }
+        public int Humidity { get; set; }
+        public float WindDirection { get; set; }
+        public float Speed { get; set; }
+        public DateTime TimeSunRise { get; set; }
+        public DateTime TimeSunSet { get; set; }
+    }
+}
---
+++ Project_1/SOA/SOA/Repository/SolarPredictionRepository.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+using SOA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOA.Repository
+{
+    public class SolarPredictionRepository : ISolarPredictionRepository
+    {
+        private readonly IMongoClient client;
+
+        public SolarPredictionRepository(IMongoClient client)
+        {
+            this.client = client;
+        }
+
+        public IEnumerable<SolarPrediction> GetSolarPredictions()
+        {
+            return client.
+        }
+    }
+}
