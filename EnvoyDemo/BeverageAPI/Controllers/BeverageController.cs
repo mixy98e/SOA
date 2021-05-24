7d5eafc781e8d46d03e93454a9acd7213d48ad50
mixy <milan.mixy.stankovic@elfak.rs>
2021-05-24 18:58:59 +0200

    EnvoyDemo: .NET API services

---
+++ EnvoyDemo/BeverageAPI/Controllers/BeverageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeverageAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BeverageController : ControllerBase
+    {
+        private static readonly string[] Drinks = new[]
+        {
+            "Beer", "Rakija", "Whiskey", "Coffee", "Tea", "Vodka", "Vine"
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rand = new Random();
+            return Ok(Drinks[rand.Next(Drinks.Length)]);
+        }
+    }
+}
---
+++ EnvoyDemo/FoodAPI/Controllers/FoodController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private static readonly string[] Food = new[]
+        {
+            "Meat", "Toast", "Burger", "Pizza", "Pasta", "Ice Cream", "Cake"
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rand = new Random();
+            return Ok(Food[rand.Next(Food.Length)]);
+        }
+    }
+}
